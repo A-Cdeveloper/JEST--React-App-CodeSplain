b42bcf50ffc7daa96ffa11b9e03ababddd69c330
Aleksandar <aleksandar@e-vlasotince.info>
2024-09-26 17:25:06 +0200

    RepositoriesSummary test

--- src/components/repositories/RepositoriesSummary.test.js
+++ src/components/repositories/RepositoriesSummary.test.js
@@ -11,20 +11,18 @@ describe('RepositoriesSummary Component', () => {
   };
 
   /////////
-  test('Dsiplay primary lang of repository', () => {
+  test('display all infos about repository', () => {
     render(<RepositoriesSummary repository={repository} />);
-    // const stargazers_count = screen.getByText(repository.stargazers_count);
-    // const open_issues = screen.getByText(
-    //   new RegExp(repository.open_issues, 'i')
-    // );
-    // const forks = screen.getByText(new RegExp(repository.forks, 'i'));
+    const stargazers_count = screen.getByText(repository.stargazers_count);
+    const open_issues = screen.getByText(
+      new RegExp(repository.open_issues, 'i')
+    );
+    const forks = screen.getByText(new RegExp(repository.forks, 'i'));
     const language = screen.getByText(/javascript/i);
 
-    // expect(stargazers_count).toBeInTheDocument();
-    // expect(open_issues).toBeInTheDocument();
-    // expect(forks).toBeInTheDocument();
+    expect(stargazers_count).toBeInTheDocument();
+    expect(open_issues).toBeInTheDocument();
+    expect(forks).toBeInTheDocument();
     expect(language).toBeInTheDocument();
-
-    // eslint-disable-next-line testing-library/no-debugging-utils
   });
 });
