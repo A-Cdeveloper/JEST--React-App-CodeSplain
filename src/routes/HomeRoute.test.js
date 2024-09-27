572411bf88db0ce91b185081e65b7c07f68b6d5c
Aleksandar <aleksandar@e-vlasotince.info>
2024-09-27 16:11:34 +0200

    HomeRoute Component test

--- src/routes/HomeRoute.test.js
+++ src/routes/HomeRoute.test.js
@@ -9,17 +9,18 @@ import HomeRoute from './HomeRoute';
 const handlers = [
   rest.get('/api/repositories', (req, res, ctx) => {
     const query = req.url.searchParams.get('q');
-    console.log(query);
+    const langName = query.split(':').at(-1);
+
     return res(
       ctx.json({
         items: [
           {
             id: 1,
-            full_name: 'Full name 1',
+            full_name: `${langName}-1`,
           },
           {
             id: 2,
-            full_name: 'Other name ',
+            full_name: `${langName}-2`,
           },
         ],
       })
@@ -34,7 +35,38 @@ afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
 
 describe('HomeRoute Component', () => {
-  it('renders without crashing', () => {
-    // render(<HomeRoute />);
+  it('renders two links for eache table/language', async () => {
+    render(
+      <MemoryRouter>
+        <HomeRoute />
+      </MemoryRouter>
+    );
+
+    const languages = [
+      'javascript',
+      'typescript',
+      'rust',
+      'go',
+      'python',
+      'java',
+    ];
+
+    languages.forEach(async (language) => {
+      const heading = await screen.findByRole('heading', {
+        name: new RegExp(language, 'i'),
+      });
+      expect(heading).toBeInTheDocument();
+
+      const links = await screen.findAllByRole('link', {
+        name: new RegExp(`${language}-`, 'i'),
+      });
+      expect(links).toHaveLength(2);
+      expect(links[0]).toHaveAttribute('href', `/repositories/${language}-1`);
+      expect(links[1]).toHaveAttribute('href', `/repositories/${language}-2`);
+    });
+    // await pause(1000);
+    // screen.debug();
   });
 });
+//
+//const pause = async (ms) => new Promise((resolve) => setTimeout(resolve, ms));
